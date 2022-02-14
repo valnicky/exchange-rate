8903bd8622d7507adbe604dcd83a28601e54172b
valnicky <valery0007@gmail.com>
2022-02-14 10:31:55 +0100

    button

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,37 +7,40 @@ class Button extends React.Component {
         this.text= React.createRef();
         this.priceBox= React.createRef();
             this.state = {
-                message: 'Loading...'
+                message: 'Loading...'     
              }   
+                this.func = this.func.bind(this);
         }
    
     func(){
         const    request = superagent;
-        const  API_KEY = "604f7e63bed2fb28f2541e0997367c09";
+      //  const  API_KEY = "604f7e63bed2fb28f2541e0997367c09";
         const API_ENDPOINT = `http://data.fixer.io/api/latest?access_key=604f7e63bed2fb28f2541e0997367c09`;
     
         request.get(API_ENDPOINT)
         .then(function(response){
             const responseObject = response.body;
             console.log(responseObject);
-        let UsdToMxn = responseObject.rates.MXN;
+            let UsdToMxn = responseObject.rates.MXN;
          //   const priceBox = document.querySelector("#price");
-                console.log(UsdToMxn);
+                console.log('usd '+UsdToMxn);
                    
-               this.props.priceBox = UsdToMxn;
-                this.setState({message: this.props.message});
-                console.log(this.props.message);
+             // this.props.priceBox.innerHtml = UsdToMxn;
+              this.setState({message:  'hello'});
+              //this.setState({message: this.state.message});
+              //this.priceBox.textContent = UsdToMxn;
+              
         })
-        .catch(function(){
-        alert("Ooops!!!"); 
+        .catch(function(err){
+        alert("Ooops!!!" + err); 
         })
     }
     
     render(){
   return (
     <div>
-         <button onClick={this.func.bind(this)} className="btn" ref={this.text}>Tell me the price!</button>
-          <div id="price" className="price"  ref={this.priceBox}>{this.state.message}</div>
+         <button onClick={this.func.bind(this)} className='btn' ref={this.text}>Tell me the price!</button>
+          <div id="" className="price"  ref={this.priceBox}>{this.state.message}</div>
           <div>{this.props.priceBox}</div>
      </div>
   )
